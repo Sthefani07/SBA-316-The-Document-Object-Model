207a8e6fbb2181f50059cfd21d337b51ab865b18
Sthefani Denhof <sdenhof7@gmail.com>
2024-09-25 16:24:46 -0700

    ForEach loop to update display based on button clicks.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll(".btn, .operator-btn");
 
@@ -6,6 +7,18 @@ buttons.forEach(button => {
     button.addEventListener("click", () => {
         const value = button.textContent;
 
+        if (value === "C") {
+            display.value = "";
+        }else if (value === "="){
+                try{
+                    display.value = eval(display.value);
+                }catch(error){
+                    display.value = "Error";
+                }
+            }else{
+                display.value += value;
+            }
         console.log(value)
     })
 });
+})
